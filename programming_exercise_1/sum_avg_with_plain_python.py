d534537a425c3f0c40cd33a0694452bfc28bc400
hoangvu1203 <bvhoangvu@gmail.com>
2025-07-28 10:05:00 +0700

    style: enhance code readability in plain Python calculator

--- programming_exercise_1/sum_avg_with_plain_python.py
+++ programming_exercise_1/sum_avg_with_plain_python.py
@@ -1,28 +1,25 @@
-# Create a multi-dimensional list to store data
-data = [[11, 12, 5, 2], [15, 6, 10], [10, 8, 12, 5], [12, 15, 8, 6]]
+# Initialize lists to store data
+data = [[11, 12, 5, 2],                             # Initialize a multi-dimensional list to store data
+        [15, 6, 10], 
+        [10, 8, 12, 5], 
+        [12, 15, 8, 6]]         
+all_numbers = []                                    # Initialize an empty list to store all numbers
+even_numbers = []                                   # Initialize an empty list to store even numbers
 
-# Initialize an empty list to store all numbers
-all_numbers = []    
-# Flatten all numbers into a single Series
-for row in data:
+# Data Preprocessing
+for row in data:                                    # Flatten all numbers into a single Series
     for num in row:
         all_numbers.append(num) 
 
-# Initialize an empty list to store even numbers
-even_numbers = []
-# Check if the number is even
-for num in all_numbers:
+for num in all_numbers:                             # Check if the number is even
     if num % 2 == 0:
         even_numbers.append(num)
 
-# 1. Sum of all numbers
-total_sum = sum(all_numbers)
-# 2. Average of all numbers by dividing the sum by the number of elements
-total_avg = sum(all_numbers) / len(all_numbers)
-# 3. Sum of all even numbers
-even_sum = sum(even_numbers)
-# 4. Average of all even numbers by dividing the sum by the number of elements
-even_avg = sum(even_numbers) / len(even_numbers)
+# Calculation
+total_sum = sum(all_numbers)                        # Sum of all numbers
+total_avg = sum(all_numbers) / len(all_numbers)     # Average of all numbers
+even_sum = sum(even_numbers)                        # Sum of all even numbers
+even_avg = sum(even_numbers) / len(even_numbers)    # Average of all even numbers
 
 # Display results
 print("Using no pandas:")
